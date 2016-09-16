34d3bf4a922b461230621aeb0bb387a583b6599b
Carolyn Kelley <CeaMonster@users.noreply.github.com>
2016-09-15 19:01:58 -0700

    Update ReverseString.js

--- ReverseString.js
+++ ReverseString.js
@@ -7,3 +7,10 @@ function reverseString(str) {
 }
 
 reverseString("hello");
+
+//Simple version
+function reverseString(str) {
+  return str.split("").reverse().join("");  
+}
+
+reverseString("hello");
