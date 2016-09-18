adb01ed0e22bf4f34535ca4dbe684e1932a0ddf0
Carolyn Kelley <CeaMonster@users.noreply.github.com>
2016-09-18 15:49:05 -0700

    Update ReturnLargestNumbers.js

--- ReturnLargestNumbers.js
+++ ReturnLargestNumbers.js
@@ -1,14 +1,18 @@
 function largestOfFour(arr) {
-  // You can do this!s
-  var largestArr = 0;
-  
-  for (i = 0; i < arr.length; i++) {
-     
-    for (j = 0; j < arr[i].length; i++) {
-      largestArr = arr[i][j];
+  var results = [];
+  for (var i = 0; i < arr.length; i++) {
+    var largestNumber = 0;
+    for (var j = 0; j < arr[i].length; j++) {
+      if (arr[i][j] > largestNumber) {
+        largestNumber = arr[i][j];
+      }
     }
-  }  
-  return largestArr;
+
+    results[i] = largestNumber;
+  }
+
+  return results;
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
